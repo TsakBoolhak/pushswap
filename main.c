acc8542259cdee9778519a1de7e44c6bf2eb5aba
TsakBoolhak <tsakboolhak@gmail.com>
2021-06-24 05:40:02 +0200

    sort_five was bad, finally fixed it, also fixed negative numbers causing segfault, everything should now work properly (need to doble check this), added a list to hold instruction, need to simplify what can be simplified in it (like ra rra -> delete, or ra rb -> rr etc) then last opti step would be to implement to fact to test 3 or 4 different chunk cuttings and keep the one which has the best result, then i'll have to do the norm and test and retest the validity of the project

--- main.c
+++ main.c
@@ -36,114 +36,148 @@ void	ft_print_int_data(void *data)
 	printf("%d\n", *(int *)data);
 }
 
-void	ft_swap_a(t_list *lst[2])
+void	ft_print_str_data(void *data)
+{
+	ft_putendl_fd((char *)(data), 1);
+}
+
+int	ft_add_instruction(t_list *lst[3], char *str)
+{
+	char	*copy;
+	t_list	*new_node;
+
+	copy = ft_strdup(str);
+	new_node = ft_lstnew(copy);
+	if (!copy || !new_node)
+	{
+		free(copy);
+		return (1);
+	}
+	ft_lstadd_back(&lst[2], new_node);
+	return (0);
+}
+
+int	ft_swap_a(t_list *lst[3])
 {
 	t_list	*tmp;
 
-	printf("sa\n");
+	if (ft_add_instruction(lst, "sa"))
+		return (1);
 	if (!(lst[0]) || !((lst[0])->next))
-		return ;
+		return (0);
 	tmp = lst[0];
 	lst[0] = (lst[0])->next;
 	tmp->next = (lst[0])->next;
 	(lst[0])->next = tmp;
+	return (0);
 }
 
-void	ft_swap_b(t_list *lst[2])
+int	ft_swap_b(t_list *lst[3])
 {
 	t_list	*tmp;
 
-	printf("sb\n");
+	if (ft_add_instruction(lst, "sb"))
+		return (1);
 	if (!(lst[1]) || !((lst[1])->next))
-		return ;
+		return (0);
 	tmp = lst[1];
 	lst[1] = (lst[1])->next;
 	tmp->next = (lst[1])->next;
 	(lst[1])->next = tmp;
+	return (0);
 }
 
-void	ft_swap_both(t_list *lst[2])
+int	ft_swap_both(t_list *lst[3])
 {
-	printf("ss\n");
-	ft_swap_a(lst);
-	ft_swap_b(lst);
+	if(ft_add_instruction(lst, "ss") || ft_swap_a(lst) || ft_swap_b(lst))
+		return (1);
+	return (0);
 }
 
-void	ft_push_a(t_list *lst[2], size_t size[2])
+int	ft_push_a(t_list *lst[3], size_t size[2])
 {
 	t_list	*to_push;
 
-	printf("pa\n");
+	if (ft_add_instruction(lst, "pa"))
+		return (1);
 	if (!lst[1])
-		return ;
+		return (0);
 	to_push = lst[1];
 	lst[1] = to_push->next;
 	to_push->next = NULL;
 	ft_lstadd_front(&lst[0], to_push);
 	size[0]++;
 	size[1]--;
+	return (0);
 }
 
-void	ft_push_b(t_list *lst[2], size_t size[2])
+int	ft_push_b(t_list *lst[3], size_t size[2])
 {
 	t_list	*to_push;
 
-	printf("pb\n");
+	if (ft_add_instruction(lst, "pb"))
+		return (1);
 	if (!lst[0])
-		return ;
+		return (0);
 	to_push = lst[0];
 	lst[0] = to_push->next;
 	to_push->next = NULL;
 	ft_lstadd_front(&lst[1], to_push);
 	size[1]++;
 	size[0]--;
+	return (0);
 }
 
-void	ft_rotate_a(t_list *lst[2])
+int	ft_rotate_a(t_list *lst[3])
 {
 	t_list	*tmp;
 	t_list	*last;
 
-	printf("ra\n");
+	if (ft_add_instruction(lst, "ra"))
+		return (1);
 	if (!(lst[0]) || !((lst[0])->next))
-		return ;
+		return (0);
 	tmp = lst[0];
 	last = ft_lstlast(tmp);
 	lst[0] = tmp->next;
 	tmp->next = NULL;
 	last->next = tmp;
+	return (0);
 }
 
-void	ft_rotate_b(t_list *lst[2])
+int	ft_rotate_b(t_list *lst[3])
 {
 	t_list	*tmp;
 	t_list	*last;
 
-	printf("rb\n");
+	if (ft_add_instruction(lst, "rb"))
+		return (1);
 	if (!(lst[1]) || !((lst[1])->next))
-		return ;
+		return (0);
 	tmp = lst[1];
 	last = ft_lstlast(tmp);
 	lst[1] = tmp->next;
 	tmp->next = NULL;
 	last->next = tmp;
+	return (0);
 }
 
-void	ft_rotate_both(t_list *lst[2])
+int	ft_rotate_both(t_list *lst[3])
 {
-	printf("rr\n");
-	ft_rotate_a(lst);
-	ft_rotate_b(lst);
+	if (ft_add_instruction(lst, "rr") || ft_rotate_a(lst) || ft_rotate_b(lst))
+		return (1);
+	return (0);
 }
 
-void	ft_reverse_rotate_a(t_list *lst[2])
+int	ft_reverse_rotate_a(t_list *lst[3])
 {
 	t_list	*last;
 	t_list	*tmp;
 
-	printf("rra\n");
+	if (ft_add_instruction(lst, "rra"))
+		return (1);
 	if (!(lst[0]) || !((lst[0])->next))
-		return ;
+		return (0);
 	tmp = lst[0];
 	last = ft_lstlast(tmp);
 	while (tmp->next != last)
@@ -151,16 +185,18 @@ void	ft_reverse_rotate_a(t_list *lst[2])
 	last->next = lst[0];
 	lst[0] = last;
 	tmp->next = NULL;
+	return (0);
 }
 
-void	ft_reverse_rotate_b(t_list *lst[2])
+int	ft_reverse_rotate_b(t_list *lst[3])
 {
 	t_list	*last;
 	t_list	*tmp;
 
-	printf("rrb\n");
+	if (ft_add_instruction(lst, "rrb"))
+		return (1);
 	if (!(lst[1]) || !((lst[1])->next))
-		return ;
+		return (0);
 	tmp = lst[1];
 	last = ft_lstlast(tmp);
 	while (tmp->next != last)
@@ -168,16 +204,19 @@ void	ft_reverse_rotate_b(t_list *lst[2])
 	last->next = lst[1];
 	lst[1] = last;
 	tmp->next = NULL;
+	return (0);
 }
 
-void	ft_reverse_rotate_both(t_list *lst[2])
+int	ft_reverse_rotate_both(t_list *lst[2])
 {
-	printf("rrr\n");
-	ft_reverse_rotate_a(lst);
-	ft_reverse_rotate_b(lst);
+	if (ft_add_instruction(lst, "rrr") || ft_reverse_rotate_a(lst))
+		return (1);
+	else if (ft_reverse_rotate_b(lst))
+		return (1);
+	return (0);
 }
 
-int	ft_check_sort_a(t_list *lst[2], size_t size[2])
+int	ft_check_sort_a(t_list *lst[3], size_t size[2])
 {
 	int		nb;
 	t_list	*tmp;
@@ -230,8 +269,11 @@ int	ft_get_max(t_list *lst, int *max_index)
 	int ret;
 	int	index;
 
-	ret = *(int *)(lst->content);
+	ret = 0;
+	if (lst)
+		ret = *(int *)(lst->content);
 	index = 1;
+	*max_index = 1;
 	while (lst)
 	{
 		if (*(int *)(lst->content) > ret)
@@ -254,10 +296,12 @@ int	ft_get_min(t_list *lst, int *min_index)
 	if (lst)
 		ret = *(int *)(lst->content);
 	index = 1;
+	*min_index = 1;
 	while (lst)
 	{
 		if (*(int *)(lst->content) < ret)
 		{
+//			printf("index = %d ret = %d content = %d\n", index, ret, *(int*)(lst->content));
 			ret = *(int *)(lst->content);
 			*min_index = index;
 		}
@@ -274,11 +318,16 @@ int	ft_search_for_lowest(t_list *lst, int nb)
 	int	result;
 
 	index = 1;
-	lowest = ft_get_max(lst, &result);
-	if (lowest > nb)
-		lowest = ft_get_min(lst, &result); 
+	result = 0;
 //	printf("nb to insert %d\n", nb);
+	lowest = ft_get_max(lst, &result);
 //	printf("search lowest : lowest at start %d result %d\n", lowest, result);
+	if (lowest < nb)
+	{
+		lowest = ft_get_min(lst, &result); 
+//		printf("search lowest : lowest at end %d result %d\n", lowest, result);
+		return (result);
+	}
 	while (lst)
 	{
 		if (nb < *(int *)(lst->content) && lowest > *(int *)(lst->content))
@@ -299,8 +348,10 @@ int	ft_search_for_highest(t_list *lst, int nb)
 	int index;
 	int	result;
 
+	result = 1;
 	index = 1;
 	highest = ft_get_min(lst, &result);
+//	printf("highest = %d, nb = %d\n", highest, nb);
 	if (highest > nb)
 		highest = ft_get_max(lst, &result); 
 //	printf("nb to insert %d\n", nb);
@@ -319,7 +370,7 @@ int	ft_search_for_highest(t_list *lst, int nb)
 	return (result);
 }
 
-void	ft_prepare_pa(t_list *lst[2], size_t size[2])
+int	ft_prepare_pa(t_list *lst[3], size_t size[2])
 {
 	int	index;
 
@@ -329,11 +380,13 @@ void	ft_prepare_pa(t_list *lst[2], size_t size[2])
 //	printf("index = %d\n", index);
 	if (index > (int)(size[0] / 2 + size[0] % 2))
 	{
-		index = (int)(size[0]) - index;
+		index = (int)(size[0]) - index + 1;
 //		printf("index = %d\n", index);
 		while (index)
 		{
-			ft_reverse_rotate_a(lst);
+//			printf("rra\n");
+			if (ft_reverse_rotate_a(lst))
+				return (1);
 			index--;
 		}
 	}
@@ -341,14 +394,17 @@ void	ft_prepare_pa(t_list *lst[2], size_t size[2])
 	{
 		while (index > 1)
 		{
-			ft_rotate_a(lst);
+//			printf("ra\n");
+			if (ft_rotate_a(lst))
+				return (1);
 			index--;
 		}
 	}
 //	printf("pa is preped\n");
+	return (0);
 }
 
-void	ft_prepare_pb(t_list *lst[2], size_t size[2])
+int	ft_prepare_pb(t_list *lst[3], size_t size[2])
 {
 	int	index;
 
@@ -361,7 +417,8 @@ void	ft_prepare_pb(t_list *lst[2], size_t size[2])
 		while (index > 0)
 		{
 //			printf("index = %d\n", index);
-			ft_reverse_rotate_b(lst);
+			if (ft_reverse_rotate_b(lst))
+				return (1);
 			index--;
 		}
 	}
@@ -369,56 +426,66 @@ void	ft_prepare_pb(t_list *lst[2], size_t size[2])
 	{
 		while (index > 1)
 		{
-			ft_rotate_b(lst);
+			if (ft_rotate_b(lst))
+				return (1);
 			index--;
 		}
 	}
 //	printf("pb is preped\n");
+	return (0);
 }
 
-void	ft_sort_three(t_list *lst[2], size_t size[2])
+int	ft_sort_three(t_list *lst[3], size_t size[2])
 {
 	int	min;
 	int	max;
+	int	ret;
 
+	ret = 0;
 	if (ft_check_sort_a(lst, size))
-		return ;
+		return (ret);
 	ft_search_minmax(lst[0], &min, &max);
 	if (size[0] == 3 && max == 2)
-		ft_reverse_rotate_a(lst);
+		ret = ft_reverse_rotate_a(lst);
 	else if (size[0] == 3 && max == 1)
-		ft_rotate_a(lst);
-	if (!ft_check_sort_a(lst, size))
-		ft_swap_a(lst);
+		ret = ft_rotate_a(lst);
+	if (ret)
+		return (ret);
+	else if (!ft_check_sort_a(lst, size))
+		ret = ft_swap_a(lst);
+	return (ret);
 }
 
-void	ft_sort_five(t_list *lst[2], size_t size[2])
+int	ft_sort_five(t_list *lst[3], size_t size[2])
 {
 	int	min;
 	int	max;
+	int	ret;
 
+	ret = 0;
 	if (ft_check_sort_a(lst, size))
-		return ;
-	while (size[0] > 3)
-		ft_push_b(lst, size);
-	ft_sort_three(lst, size);
-	while (size[1])
-	{
-		ft_prepare_pa(lst, size);
-		ft_push_a(lst,size);
-	}
+		return (0);
+	while (!ret && size[0] > 3)
+		ret = ft_push_b(lst, size);
+	if (ret || ft_sort_three(lst, size))
+		return (1);
+	while (size[1] && !ret && !ft_prepare_pa(lst, size))
+		ret = ft_push_a(lst,size);
+	if (ret || size[1])
+		return (1);
 	ft_search_minmax(lst[0], &min, &max);
 	if (min > (int)(size[0] / 2 + size[0] % 2))
 	{
 		min = (int)(size[0] - min + 2);
-		while (--min)
-			ft_reverse_rotate_a(lst);
+		while (--min && !ret)
+			ret = ft_reverse_rotate_a(lst);
 	}
 	else
 	{
-		while (--min)
-			ft_rotate_a(lst);
+		while (--min && !ret)
+			ret = ft_rotate_a(lst);
 	}
+	return (ret);
 }
 
 void ft_swap(int *a, int *b)
@@ -551,7 +618,7 @@ int	ft_pick_from_end(t_list *lst, int *arr, int chunk_i, int chunk_size)
 int	main(int ac, char *av[])
 {
 	int		i;
-	t_list	*lst[2];
+	t_list	*lst[3];
 	size_t	size[2];
 	char	**tab;
 	int		*arr;
@@ -565,8 +632,10 @@ int	main(int ac, char *av[])
 	if (ac < 2)
 		return (0);
 	i = 1;
+	arr = NULL;
 	lst[0] = NULL;
 	lst[1] = NULL;
+	lst[2] = NULL;
 	ft_memset(size, 0, sizeof(size));
 	if (ac == 2)
 	{
@@ -606,14 +675,6 @@ int	main(int ac, char *av[])
 	}
 	size[0] = ft_lstsize(lst[0]);
 
-	arr_size = size[0];
-	arr = ft_get_arr_from_list(lst[0], arr_size);
-	if (!arr)
-	{
-		ft_putstr_fd("Error: not enough memory\n", 2);
-		ft_lstclear(&lst[0], &free);
-		return (0);
-	}
 //	ft_print_array(arr, arr_size);
 
 
@@ -627,16 +688,38 @@ int	main(int ac, char *av[])
 
 	if (size[0] < 4)
 	{
-		ft_sort_three(lst, size);
+		if (ft_sort_three(lst, size))
+		{
+			ft_putstr_fd("Error: not enough memory\n", 2);
+			ft_lstclear(&lst[0], &free);
+			ft_lstclear(&lst[1], &free);
+			ft_lstclear(&lst[2], &free);
+			return (0);
+		}
 	}
 	else if (size[0] < 6)
 	{
-		ft_sort_five(lst, size);
+		if (ft_sort_five(lst, size))
+		{
+			ft_putstr_fd("Error: not enough memory\n", 2);
+			ft_lstclear(&lst[0], &free);
+			ft_lstclear(&lst[1], &free);
+			ft_lstclear(&lst[2], &free);
+			return (0);
+		}
 	}
 	else
 	{
+		arr_size = size[0];
+		arr = ft_get_arr_from_list(lst[0], arr_size);
+		if (!arr)
+		{
+			ft_putstr_fd("Error: not enough memory\n", 2);
+			ft_lstclear(&lst[0], &free);
+			return (0);
+		}
 		if (size[0] < 250)
-			chunk_size = size[0] / 7;
+			chunk_size = size[0] / 7 + (size[0] / 7 < 1);
 		else
 			chunk_size = size[0] / 14;
 		chunk_nb = arr_size / chunk_size + (arr_size % chunk_size != 0);
@@ -659,7 +742,15 @@ int	main(int ac, char *av[])
 			{
 				while (first_chosen)
 				{
-					ft_rotate_a(lst);
+					if (ft_rotate_a(lst))
+					{
+						free(arr);
+						ft_putstr_fd("Error: not enough memory\n", 2);
+						ft_lstclear(&lst[0], &free);
+						ft_lstclear(&lst[1], &free);
+						ft_lstclear(&lst[2], &free);
+						return (0);
+					}
 					first_chosen--;
 				}
 			}
@@ -667,12 +758,27 @@ int	main(int ac, char *av[])
 			{
 				while (last_chosen)
 				{
-					ft_reverse_rotate_a(lst);
+					if (ft_reverse_rotate_a(lst))
+					{
+						free(arr);
+						ft_putstr_fd("Error: not enough memory\n", 2);
+						ft_lstclear(&lst[0], &free);
+						ft_lstclear(&lst[1], &free);
+						ft_lstclear(&lst[2], &free);
+						return (0);
+					}
 					last_chosen--;
 				}
 			}
-			ft_prepare_pb(lst, size);
-			ft_push_b(lst,size);
+			if (ft_prepare_pb(lst, size) || ft_push_b(lst,size))
+			{
+				ft_putstr_fd("Error: not enough memory\n", 2);
+				free(arr);
+				ft_lstclear(&lst[0], &free);
+				ft_lstclear(&lst[1], &free);
+				ft_lstclear(&lst[2], &free);
+				return (0);
+			}
 		}
 
 
@@ -682,15 +788,45 @@ int	main(int ac, char *av[])
 		{
 			chunk_nb = (int)(size[1] - chunk_nb + 2);
 			while (--chunk_nb)
-				ft_reverse_rotate_b(lst);
+			{
+				if (ft_reverse_rotate_b(lst))
+				{
+					free(arr);
+					ft_putstr_fd("Error: not enough memory\n", 2);
+					ft_lstclear(&lst[0], &free);
+					ft_lstclear(&lst[1], &free);
+					ft_lstclear(&lst[2], &free);
+					return (0);
+				}
+			}
 		}
 		else
 		{
 			while (--chunk_nb)
-				ft_rotate_b(lst);
+			{
+				if (ft_rotate_b(lst))
+				{
+					free(arr);
+					ft_putstr_fd("Error: not enough memory\n", 2);
+					ft_lstclear(&lst[0], &free);
+					ft_lstclear(&lst[1], &free);
+					ft_lstclear(&lst[2], &free);
+					return (0);
+				}
+			}
 		}
 		while (size[1])
-			ft_push_a(lst, size);
+		{
+			if (ft_push_a(lst, size))
+			{
+				free(arr);
+				ft_putstr_fd("Error: not enough memory\n", 2);
+				ft_lstclear(&lst[0], &free);
+				ft_lstclear(&lst[1], &free);
+				ft_lstclear(&lst[2], &free);
+				return (0);
+			}
+		}
 	}
 //	printf("\nafter the sort:\n");
 //	printf("list a :\n");
@@ -926,8 +1062,10 @@ int	main(int ac, char *av[])
 	printf("list b's size : %zu\n", size[1]);
 	*/
 
+	ft_lstiter(lst[2], &ft_print_str_data);
 	free(arr);
 	ft_lstclear(&lst[0], &free);
 	ft_lstclear(&lst[1], &free);
+	ft_lstclear(&lst[2], &free);
 	return (0);
 }
